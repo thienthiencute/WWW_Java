3dff1dc15e780b19e2871e11b244779b40466a69
thienthien <120097680+thienthiencute@users.noreply.github.com>
2025-10-04 14:27:10 +0700

    Delete 22687721_ThanHoangThienThien_SpingJpa directory

--- 22687721_ThanHoangThienThien_SpingJpa/src/main/java/fit/iuh/thanhoangthienthien_spingjpa/repository/DepartmentRepository.java
+++
@@ -1,7 +0,0 @@
-package fit.iuh.thanhoangthienthien_spingjpa.repository;
-
-import fit.iuh.thanhoangthienthien_spingjpa.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
-}
